ed8c982ea9a9084c3f8f6c0c916b6095a77f391b
Fabian Fabro <fabrofabian@gmail.com>
2019-11-01 22:40:14 -0700

    classandobject

---
+++ Loops/ClassAndObject/ClassAndObject.cpp
@@ -0,0 +1,53 @@
+// ClassAndObject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class House
+{
+	private:
+		int walls;
+		int roofs;
+		int doors;
+		string material;
+
+	public:
+		void showWalls(int w)
+		{
+			walls = w;
+			cout << "Number of walls: " << walls << endl;
+		}
+		int showRoofs()
+		{
+			cout << "How many roofs? " << endl;
+			cin >> roofs;
+			return roofs;
+		}
+};
+
+int main()
+{
+    cout << "Classes & Objects" << endl; 
+
+	House h1, h2;
+	int input;
+	h1.showWalls(4);
+	input = h2.showRoofs();
+
+	cout << "You entered " << input << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
